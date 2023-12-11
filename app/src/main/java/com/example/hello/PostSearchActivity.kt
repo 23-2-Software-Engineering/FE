6780068a56121d170461b3f1980bb9cba2efb576
Gunwoo <gunwuu512@gmail.com>
2023-12-11 21:09:32 +0900

    feat: 게시글 페이지 레이아웃 수정 및 좋아요 순서대로 게시글 추천 기능 추가

--- app/src/main/java/com/example/hello/PostSearchActivity.kt
+++ app/src/main/java/com/example/hello/PostSearchActivity.kt
@@ -3,23 +3,14 @@ package com.example.hello
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.widget.AdapterView
 import android.widget.GridView
-import android.widget.Toast
-import androidx.appcompat.app.AlertDialog
-import com.example.hello.R
 import com.example.hello.adapter.PostSearchAdapter
 import com.example.hello.api.PostSearchService
 import com.example.hello.api.RetrofitClient
 import com.example.hello.databinding.ActivityPostSearchBinding
-import com.example.hello.model.CourseInfo
 import com.example.hello.model.PostDTO
-import com.example.hello.model.PostDataDTO
 import com.google.android.material.textfield.TextInputEditText
-import kotlinx.android.synthetic.main.activity_post_search.tietSearch
 import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
 import retrofit2.Retrofit
 import java.io.IOException
 import java.lang.NullPointerException
@@ -28,7 +19,6 @@ class PostSearchActivity : AppCompatActivity() {
 
     private val postSearchBinding by lazy { ActivityPostSearchBinding.inflate(layoutInflater) }
     private lateinit var gridView: GridView
-    private lateinit var adapter: PostSearchAdapter
     private var postList = ArrayList<PostDTO>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -38,24 +28,16 @@ class PostSearchActivity : AppCompatActivity() {
 
         gridView = findViewById(R.id.postGridView)
 
-//        val dummyPostList = ArrayList<PostSearchTestDTO>()
-//        for(i: Int in 1..8) {
-//            dummyPostList.add(PostSearchTestDTO("Post Title${i}"))
-//        }
-//        gridView.adapter = PostSearchTestAdapter(this, dummyPostList)
-
-        // 모든 게시글 검색
         try {
-            searchAllPost()
-        } catch (e :NullPointerException) {
-            Log.e("SEARCH POST", "ERR: 포스트 찾기에서 NULL POINTER EXCEPTION 발생")
+            // 모든 게시글 검색 리퀘스트
+            // searchAllPost()
+
+            // 게시글 추천 리퀘스트
+            recommendPost()
+        } catch (e: NullPointerException) {
+            Log.e("RECOMMEND POST", "ERR: 포스트 추천에서 NULL POINTER EXCEPTION 발생")
         }
         gridView.adapter = PostSearchAdapter(this, postList)
-
-        // 포스트 이미지 클릭
-        gridView.setOnItemClickListener{parent, view, position, id ->
-
-        }
 
         // 뒤로 가기 버튼 클릭
         postSearchBinding.backBtn.setOnClickListener {
@@ -65,24 +47,53 @@ class PostSearchActivity : AppCompatActivity() {
         // 검색 버튼 클릭
         postSearchBinding.searchRequestBtn.setOnClickListener {
             val tietSearch: TextInputEditText = findViewById(R.id.tietSearch)
-            searchPostByTag(tietSearch.text.toString())
+            if (!tietSearch.text!!.isEmpty()) {
+                searchPostByTag(tietSearch.text.toString())
+            } else {
+                recommendPost()
+            }
+
             gridView.adapter = PostSearchAdapter(this, postList)
         }
-
 
     }
 
     // 포스트 전체 검색
     private fun searchAllPost() {
         var retrofit: Retrofit = RetrofitClient.getInstance()
-        var courseSearchService = retrofit.create(PostSearchService::class.java)
+        var postSearchService = retrofit.create(PostSearchService::class.java)
 
-        val callSync: Call<ArrayList<PostDTO>> = courseSearchService.searchPostAll()
+        val callSync: Call<ArrayList<PostDTO>> = postSearchService.searchPostAll()
         Thread(Runnable() {
             try {
                 postList = callSync.execute().body()!!
             } catch (e: IOException) {
                 Log.e("SEACRCH POST ALL", "ERR: " + callSync.execute().errorBody())
+                e.printStackTrace()
+            }
+        }).start()
+
+        try {
+            Thread.sleep(1000);
+        } catch (e: InterruptedException) {
+            e.printStackTrace()
+        }
+
+        Log.d("SEARCH POST ALL", "MSG: " + postList.toString())
+    }
+
+    // 게시글 좋아요 순으로 추천
+    private fun recommendPost() {
+        var retrofit: Retrofit = RetrofitClient.getInstance()
+        var postSearchService = retrofit.create(PostSearchService::class.java)
+
+        Log.d("REQUEST RECOMMEND POST", "")
+        val callSync: Call<ArrayList<PostDTO>> = postSearchService.searchPostOrderByLikes()
+        Thread(Runnable() {
+            try {
+                postList = callSync.execute().body()!!
+            } catch (e: IOException) {
+                Log.e("RECOMMEND POST", "ERR: " + callSync.execute().errorBody())
                 e.printStackTrace()
             }
         }).start();
@@ -93,7 +104,7 @@ class PostSearchActivity : AppCompatActivity() {
             e.printStackTrace()
         }
 
-        Log.d("SEARCH POST ALL", "MSG: " + postList.toString())
+        Log.d("RECOMMEND POST", "MSG: " + postList.toString())
     }
 
     // 태그로 게시글 검색
